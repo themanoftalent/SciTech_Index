cdbe4b3f18dde97b161b4d1121974a5b6c79c215
Your Name <you@example.com>
2024-01-28 16:24:20 +0100

    .

--- add_subject_file.py
+++ add_subject_file.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 s_locale_file = 'locale.txt'
+s_subject_header_file = 'subject_header.txt'
 
 ls_locale = Path(s_locale_file).read_text().splitlines()
 for i_locale_index in range(len(ls_locale)):
@@ -12,7 +13,7 @@ for i_locale_index in range(len(ls_locale)):
 		ls_locale[i_locale_index] = s_anchor_locale
 		break
 
-s_text = 'locale\tsubject\n'
+s_text = Path(s_subject_header_file).read_text().strip() + '\n'
 for s_locale_entry in ls_locale:
 	s_text += s_locale_entry + '\t\n'
 
@@ -20,19 +21,11 @@ top_path = Path.cwd()
 for s_file_directory_path in glob(f'{top_path}/**/*/', recursive=True):
 	s_file_name = '_' + s_file_directory_path[s_file_directory_path.rstrip('/').rfind('/')+1:].rstrip('/') + '_.tsv'
 
-	ls_synonym_separator = [',', '،', '꛵', '、', '，', '፣', '𖬹', '꓾', '𖺗', '᠂', '᠈', '𑑍', '߸', '𝪇', '༔', '꘍']
+	ls_synonym_separator = [', ', '، ', '꛵ ', '、', '，', '፣ ', '𖬹 ', '꓾ ', '𖺗 ', ' ᠂ ', ' ᠈ ', '𑑍 ', '߸ ', '𝪇', '༔ ', '꘍ ']
 	for s_synonym_separator in ls_synonym_separator:
-		if s_synonym_separator in ['、', '，']:
-			s_file_name = s_file_name.replace(s_synonym_separator, '_' + s_synonym_separator + '_')
-			s_file_name = s_file_name.replace('_' + s_synonym_separator + '__' + s_synonym_separator + '_', s_synonym_separator*2)
-		elif s_synonym_separator in ['᠂', '᠈']:
-			s_file_name = s_file_name.replace(' ' + s_synonym_separator + ' ', '_ ' + s_synonym_separator + ' _')
-			s_file_name = s_file_name.replace('_ ' + s_synonym_separator + ' __ ' + s_synonym_separator + ' _', ' ' + s_synonym_separator*2 + ' ')
-		else:
-			s_file_name = s_file_name.replace(s_synonym_separator + ' ', '_' + s_synonym_separator + ' _')
-			s_file_name = s_file_name.replace(s_synonym_separator + '_' + s_synonym_separator + ' _', s_synonym_separator*2 + ' ')
+		s_file_name = s_file_name.replace(s_synonym_separator, '_' + s_synonym_separator + '_')
+		s_file_name = s_file_name.replace('_' + s_synonym_separator + '__' + s_synonym_separator + '_', s_synonym_separator*2)
 	file_path = Path(s_file_directory_path).joinpath(s_file_name)
 	if not file_path.is_file():
 		print(file_path)
-		s_text = s_text.replace(s_anchor_locale + '\t', s_anchor_locale + '\t' + s_file_name.removesuffix('.tsv'))
-		file_path.write_text(s_text)
+		file_path.write_text(s_text.replace(s_anchor_locale + '\t', s_anchor_locale + '\t' + s_file_name.removesuffix('.tsv')))
